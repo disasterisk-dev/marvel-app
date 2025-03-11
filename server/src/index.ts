4cf77508d00b201e6b4696e501c02e7a1231a157
JoelRobinsonUK <joelsgrobinson@gmail.com>
2025-03-11 17:27:57 +0000

    Added schemas and scripts for monorepo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,35 @@
 import { Elysia } from "elysia";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import { entries } from "./db/schema";
+import { eq } from "drizzle-orm";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const client = createClient({
+  url: process.env.DB_URL!,
+  authToken: process.env.DB_TOKEN!,
+});
+
+export const db = drizzle({ client });
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/movies", async () => {
+    const data = await db
+      .select()
+      .from(entries)
+      .where(eq(entries.medium, "Movie"));
+
+    return data;
+  })
+  .get("/movies/:id", async ({ params }) => {
+    const data = await db
+      .select()
+      .from(entries)
+      .where(eq(entries.id, parseInt(params.id)));
+
+    return data[0];
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
