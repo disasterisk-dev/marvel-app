25958e02ce22b0aa5caff255890e9044c1076508
JoelRobinsonUK <joelsgrobinson@gmail.com>
2025-04-15 13:27:06 +0100

    Director multi select
    Can now select from existing directors or create a new one!

--- client/src/components/forms/EntryForm.tsx
+++ client/src/components/forms/EntryForm.tsx
@@ -3,12 +3,16 @@ import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { useForm } from "@tanstack/react-form";
 import { FieldInfo } from "./FieldInfo";
+import AsyncCreatableSelect from "react-select/async-creatable";
+import axios from "axios";
+import { entry } from "@/types";
 
 const EntryForm = () => {
   const form = useForm({
     defaultValues: {
       title: "",
-      password: "",
+      releaseDate: "",
+      directors: [""],
     },
     onSubmit: async ({ value }) => {
       console.log(value);
@@ -33,7 +37,7 @@ const EntryForm = () => {
         children={(field) => {
           return (
             <>
-              <Label htmlFor={field.name}>{field.name}</Label>
+              <Label htmlFor={field.name}>Title</Label>
               <Input
                 id={field.name}
                 name={field.name}
@@ -46,27 +50,49 @@ const EntryForm = () => {
           );
         }}
       />
-      <hr />
       <form.Field
-        name="password"
-        validators={{
-          onChange: ({ value }) =>
-            value.split("").length > 0
-              ? undefined
-              : "Admin password is required to create entries",
-        }}
+        name="releaseDate"
         children={(field) => {
           return (
             <>
-              <Label htmlFor={field.name}>{field.name}</Label>
+              <Label htmlFor={field.name}>Release Date</Label>
               <Input
                 id={field.name}
                 name={field.name}
                 value={field.state.value}
-                type="password"
+                type="date"
                 onChange={(e) => field.handleChange(e.target.value)}
               />
               <FieldInfo field={field} />
+            </>
+          );
+        }}
+      />
+      <form.Field
+        name="directors"
+        validators={{
+          onChange: ({ value }) =>
+            value.length > 0
+              ? undefined
+              : "At least one director or showrunner is needed.",
+        }}
+        children={(field) => {
+          return (
+            <>
+              <Label htmlFor={field.name}>Directors</Label>
+              {/* TODO: Needs styling to match Shadcn */}
+              <AsyncCreatableSelect
+                isMulti
+                cacheOptions={false}
+                id={field.name}
+                defaultOptions
+                loadOptions={loadDirectors}
+                onChange={(v) => {
+                  const values: string[] = [];
+                  v.forEach((i) => values.push(i.value));
+                  field.handleChange(values);
+                }}
+              />
             </>
           );
         }}
@@ -84,3 +110,40 @@ const EntryForm = () => {
 };
 
 export default EntryForm;
+
+interface directorOption {
+  readonly value: string;
+  readonly label: string;
+}
+
+const loadDirectors = (
+  inputValue: string,
+  callback: (options: directorOption[]) => void,
+) => {
+  axios.get(import.meta.env.VITE_API_BASE_URL + "/entries").then((res) => {
+    const entries: entry[] = res.data.items;
+    const directors: string[] = [];
+
+    // Filter out duplicates first
+    entries.forEach((e) => {
+      e.directors.forEach((d) => {
+        if (!directors.includes(d)) {
+          directors.push(d);
+        }
+      });
+    });
+
+    // convert plain strings into options for the react-select library
+    const options: directorOption[] = [];
+
+    directors.forEach((d) => {
+      options.push({ label: d, value: d });
+    });
+
+    callback(
+      options.filter((o) =>
+        o.value.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase()),
+      ),
+    );
+  });
+};
